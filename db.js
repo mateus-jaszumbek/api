699effdc889eccae97fd12a0e5fefde5524a1d7a
mateus-jaszumbek <mateusjaszumbek@hotmail.com>
2024-03-12 21:21:44 -0300

    commit

--- db.js
+++ db.js
@@ -1,7 +1,6 @@
 import mysql from "mysql";
 
 export const db = mysql.createConnection({
-	connectionLimit: 10,
 	host: "monorail.proxy.rlwy.net",
 	user: "root",
 	password: "TtSTOnumhALIdFrJsHXuzpkgkWBofXUt",
